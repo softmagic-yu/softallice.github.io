a321ff11f854f0db1434ba956669ee9786ea3bbe
s991045 <s991045@gmail.com>
2019-09-04 16:36:13 +0900

    3

---
+++ _js/src/index.js
@@ -0,0 +1,12 @@
+import 'core-js/fn/array/for-each';
+import 'core-js/fn/function/bind';
+import 'core-js/fn/number/constructor';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/object/define-property';
+import 'core-js/fn/object/keys';
+import 'core-js/fn/array/reduce';
+
+import './style';
+import './katex';
+import './drawer';
+import './push-state';
